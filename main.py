729ae39f4b9e8321ce6b437af27f5eb80ea1b53a
Vladimir Elistratov <vvoovv.projects@gmail.com>
2015-04-10 15:44:33 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,177 @@
+from pro import *
+
+from stairs import SimpleStairs
+
+height = param(10)
+platformHeight = param(2)
+railwayWidth = param(3)
+
+entranceWidth = 6
+
+
+@rule
+def Begin():
+	extrude(height,
+		left >> Side(),
+		right >> Side(),
+		front >> Wall(),
+		back >> Wall(),
+		top >> delete(),
+		bottom >> split(y,
+			railwayWidth >> Way(),
+			flt() >> extrude(platformHeight, top>>Platform()),
+			railwayWidth >> Way()
+		),
+		interior = True
+	)
+
+
+@rule
+def Platform():
+	texture("MarekFootway0007.jpg", 0.6, 0.3)
+	split(y,
+		3,
+		3 >> Columns(),
+		flt(),
+		3 >> Columns(),
+		3
+	)
+
+
+@rule
+def Columns():
+	split(x,
+		4,
+		repeat(
+			3 >> Column(),
+			flt(4)
+		),
+		4
+	)
+
+
+@rule
+def Column():
+	texture("platform_wall.jpg", 1.4, 0.7)
+	extrude(8)
+
+
+@rule
+def Wall():
+	texture("platform_wall.jpg", 14, 7)
+	split(y,
+		rel(0.3),
+		flt() >> extrude(0.4)
+	)
+
+
+@rule
+def Way():
+	split(y,
+		flt(),
+		0.1 >> Rail(),
+		0.3,
+		1.5 >> WayPit(),
+		0.3,
+		0.1 >> Rail(),
+		flt()
+	)
+
+
+@rule
+def WayPit():
+	color("222222")
+	extrude(-0.5)
+
+
+@rule
+def Rail():
+	color("eeeeee")
+	extrude(0.2)
+
+
+@rule
+def Side():
+	split(x,
+		railwayWidth >> Tunnel(),
+		flt() >> split(x,
+			flt(),
+			rel(0.8) >> split(y,
+				platformHeight >> delete(),
+				rel(0.6) >> EntranceToPlatform(),
+				flt() >> delete()
+			),
+			flt()
+		),
+		railwayWidth >> Tunnel()
+	)
+
+
+@rule
+def Tunnel():
+	split(y,
+		rel(0.8) >> delete(),
+		flt()
+	)
+
+
+@rule
+def EntranceToPlatform():
+	extrude2(
+		0, -20 >> delete(),
+		1.6, -20 >> split(y,
+			flt() >> Steps(20),
+			3 >> extrude(-20, top >> delete(), front >> PedestrianSubway())
+		),
+		1.6, -10 >> delete(),
+		last >> delete(),
+		axis = y,
+		symmetric = False
+	)
+
+
+@rule
+def Steps(_stepsLength):
+	_stepsHeight = shape().size()[1]
+	SimpleStairs(_stepsLength/_stepsHeight*0.1, 0.1, texture("MarekFootway0007.jpg", 0.6, 0.3), keepSides=False)
+
+
+@rule
+def PedestrianSubway():
+	texture("pedestrian_subway_wall.jpg", 0.8, 0.8)
+	extrude2(
+		-60, 3,
+		-60, 64,
+		-66, 64 >> Entrance(10, 5, 5),
+		-66, -3,
+		60, -7 >> split(x,
+			33.8,
+			entranceWidth >> Entrance(),
+			40,
+			entranceWidth >> Entrance(),
+			flt(),
+			entranceWidth >> Entrance(),
+			7.5
+		),
+		62, 88,
+		55, 88,
+		53, -1,
+		last >> delete(),
+		cap2 >> delete(),
+		cap1 >> texture("MarekFootway0007.jpg", 0.6, 0.3),
+		symmetric = False,
+		relativeCoord1 = False
+	)
+
+
+@rule
+def Entrance(stepsLength=10, stepsHeight=5, entranceLength=5):
+	extrude2(
+		0, -stepsLength >> delete(),
+		stepsHeight, -stepsLength >> SimpleStairs(stepsLength/stepsHeight*0.1, 0.1, texture("MarekFootway0007.jpg", 0.6, 0.3), keepSides=False),
+		stepsHeight, -stepsLength + entranceLength >> inset(0, 1, 1, 1, cap>>delete(), side>>extrude(-1)),
+		last >> delete(),
+		axis = y,
+		symmetric = False,
+		relativeCoord1 = False
+	)
